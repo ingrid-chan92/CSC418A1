0efe7a60ce2b4c601cec2e44b0b9ef974e5b0e55
VM User <vmuser@vm-ubuntu-369.(none)>
2015-03-17 00:46:32 -0400

    implemented intersect for square and sphere. Not tested

--- a3/raytracerLinux/scene_object.cpp
+++ a3/raytracerLinux/scene_object.cpp
@@ -10,6 +10,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <algorithm>
 #include "scene_object.h"
 
 bool UnitSquare::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
@@ -26,9 +27,39 @@ bool UnitSquare::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
 	// HINT: Remember to first transform the ray into object space  
 	// to simplify the intersection test.
 
-	// Related to TraverseScene
+	// Convert ray to Object space	
+	Ray3D objRay;
+	objRay.origin = worldToModel * ray.origin;
+	objRay.dir = worldToModel * ray.dir;
 
-	return false;
+	// Calculate lambda where [[0.5, -0.5, 0] - [origin - dir*lambda]] . normal = 0
+	// Simplifies to cz - dirz*lambda = 0  ==  lambda = -cz/dirz
+	float lambda = -objRay.origin[2]/objRay.dir[2];		
+	if (lambda <= 0) {
+		return false;
+	}
+
+	// Using lambda, calculate the intersection point of the ray and the square
+	float intersectX = objRay.origin[0] + (lambda * objRay.dir[0]);
+	float intersectY = objRay.origin[1] + (lambda * objRay.dir[1]);
+
+	bool isXIntersecting = -0.5 >= intersectX && intersectX <= 0.5;
+	bool isYIntersecting = -0.5 >= intersectY && intersectY <= 0.5;
+
+	// Does ray intersect with unit square and is closer to the existing intersection?
+	if (isXIntersecting && isYIntersecting && (ray.intersection.none || ray.intersection.t_value > lambda)) {		
+
+		ray.intersection.point = modelToWorld * Point3D(intersectX, intersectY, 0);
+		ray.intersection.normal = modelToWorld * Vector3D(0, 0, 1);
+		ray.intersection.normal.normalize();
+		ray.intersection.none = false;
+		ray.intersection.t_value = lambda;
+		return true;
+
+	} else {
+		// Intersecting point does not go through unit square/exists a closer point
+		return false;
+	}	
 }
 
 bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
@@ -43,8 +74,56 @@ bool UnitSphere::intersect( Ray3D& ray, const Matrix4x4& worldToModel,
 	// HINT: Remember to first transform the ray into object space  
 	// to simplify the intersection test.
 
-	// Related to TraverseScene
+	// Convert ray to Object space	
+	Ray3D objRay;
+	objRay.origin = worldToModel * ray.origin;
+	objRay.dir = worldToModel * ray.dir;
+
+	// Calculate A, B, and C for quadratic equation At^2 + 2Bt + C = 0
+	Point3D sphereOrigin(0, 0, 0);
+	float A = (objRay.dir).dot(objRay.dir);
+	float B = 2 * (objRay.origin - sphereOrigin).dot(objRay.dir);
+	float C = (objRay.origin - sphereOrigin).dot(objRay.origin - sphereOrigin) - 1;
 	
-	return false;
+	// Calculate determinant
+	float det = B*B - 4*A*C;
+
+	if (det < 0) {
+		// Does not intersect 
+		return false;
+	}
+
+	// At this point, the ray must intersect the sphere somewhere
+	float lambda;
+	float lambda1 = (-B + sqrt(det)) / 2*A;
+	float lambda2 = (-B - sqrt(det)) / 2*A;
+	
+	if (lambda1 < 0 && lambda2 < 0) {
+		// Intersections behind view plane. Not visible
+		return false;
+	} else if (lambda1 < 0 || lambda2 < 0) {
+		// Exists one intersection that is off screen
+		lambda = std::max(lambda1, lambda2);
+	} else {
+		// Both intersections on screen. Choose closer one
+		lambda = std::min(lambda1, lambda2);
+	}
+
+	if (ray.intersection.none || lambda < ray.intersection.t_value) {
+		Point3D intersection(objRay.origin + lambda*objRay.dir);
+
+		ray.intersection.point = modelToWorld * intersection;
+		ray.intersection.normal = worldToModel.transpose() * (intersection - sphereOrigin);
+		ray.intersection.normal.normalize();
+		ray.intersection.none = false;
+		ray.intersection.t_value = lambda;
+		return true;
+
+	} else {
+		// Closer intersection exists
+		return false;
+
+	}	
+
 }
 
