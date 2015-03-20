cc76ca7a20d58c71fef0bd711a4a0231664a381e
VM User <vmuser@vm-ubuntu-369.(none)>
2015-03-19 20:58:43 -0400

    Added lighting into light_source.cpp

--- a3/raytracerLinux/light_source.cpp
+++ a3/raytracerLinux/light_source.cpp
@@ -20,5 +20,32 @@ void PointLight::shade( Ray3D& ray ) {
 	// is available.  So be sure that traverseScene() is called on the ray 
 	// before this function.  
 
+	Material *mat = ray.intersection.mat;
+
+	// Calculate necessary vectors for colour calculation
+	Vector3D normal = ray.intersection.normal;
+	Vector3D pointSourceDir = _pos - ray.intersection.point;	
+	Vector3D cameraDir = -ray.dir;	
+
+	normal.normalize();
+	pointSourceDir.normalize();
+	cameraDir.normalize();
+
+	Vector3D mirrorDir = (2 * (normal.dot(pointSourceDir)) * normal) - pointSourceDir;
+	mirrorDir.normalize();
+
+	// Calculate Ambient Colour
+	Colour ambient = mat->ambient * _col_ambient;
+
+	// Calculate Diffuse Colour
+	Colour diffuse = fmax(0, normal.dot(pointSourceDir)) * mat->diffuse * _col_diffuse;
+	
+	// Calculate Specular Colour
+	float specularHighlight =  pow(fmax(0, mirrorDir.dot(cameraDir)), mat->specular_exp);
+	Colour specular = specularHighlight * mat->specular * _col_specular;
+
+	ray.col = ambient + diffuse + specular;
+	ray.col.clamp();
+
 }
 
